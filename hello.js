7dd0d693c3c117fb983aecc99e456fcd2d71b8ba
Alvin Lin <alvinch.lin@gmail.com>
2021-03-18 06:11:55 +0000

    Adding unsaved parts of hello.js

--- hello.js
+++ hello.js
@@ -6,10 +6,24 @@
 
 //-------------
 
-const sayHello  = function (name) {
+// const sayHello  = function (name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+//--------------
+// console.log:
+const sayHelloToConsole  = function (name) {
   console.log("Hello, " + name);
 }
+sayHelloToConsole('John'); 
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// return :
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
